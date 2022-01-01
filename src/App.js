1f060296f2603c4d4e95022bc55812165b8183e4
abdallah-alaff96 <abdallah96_alaff@hotmail.com>
2022-01-01 22:56:27 +0200

    define and use Route & BrowserRouter

--- src/App.js
+++ src/App.js
@@ -1,7 +1,16 @@
+import { Route } from "react-router-dom";
+import Products from "./screens/Products";
+import Welcome from "./screens/Welcome";
+
 function App() {
   return (
     <div>
-      <h2>Let's get started!</h2>
+      <Route path="/welcome">
+        <Welcome />
+      </Route>
+      <Route path="/products">
+        <Products />
+      </Route>
     </div>
   );
 }
