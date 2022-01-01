5f63d7778d44a5cf16436d8cd3621c6c3fb8ed8c
abdallah-alaff96 <abdallah96_alaff@hotmail.com>
2022-01-01 23:35:43 +0200

    working with links and using Routes with element prop (new features in version 6 of react-router-dom)

--- src/App.js
+++ src/App.js
@@ -1,16 +1,16 @@
-import { Route } from "react-router-dom";
+import { Route, Routes } from "react-router-dom";
+import MainHeader from "./components/MainHeader";
 import Products from "./screens/Products";
 import Welcome from "./screens/Welcome";
 
 function App() {
   return (
     <div>
-      <Route path="/welcome">
-        <Welcome />
-      </Route>
-      <Route path="/products">
-        <Products />
-      </Route>
+      <MainHeader />
+      <Routes>
+        <Route path="/welcome" element={<Welcome />} />
+        <Route path="/products" element={<Products />} />
+      </Routes>
     </div>
   );
 }
