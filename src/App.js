be900821fd91d769f4a54460e27150e800f6d83e
abdallah-alaff96 <abdallah96_alaff@hotmail.com>
2022-01-07 13:51:36 +0200

    change react-router-dom@5.2.0 instead of @6.2.0

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { Route, Routes } from "react-router-dom";
+import { Route } from "react-router-dom";
 import MainHeader from "./components/MainHeader";
 import Products from "./screens/Products";
 import Welcome from "./screens/Welcome";
@@ -7,10 +7,14 @@ function App() {
   return (
     <div>
       <MainHeader />
-      <Routes>
-        <Route path="/welcome" element={<Welcome />} />
-        <Route path="/products" element={<Products />} />
-      </Routes>
+      <main>
+        <Route path="/welcome">
+          <Welcome />
+        </Route>
+        <Route path="/products">
+          <Products />
+        </Route>
+      </main>
     </div>
   );
 }
