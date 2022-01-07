a03d7b29eb4db75efc28921307f01a87654f26d0
abdallah-alaff96 <abdallah96_alaff@hotmail.com>
2022-01-07 20:56:51 +0200

    Add useParams,  :/productId,  exact,  Switch  and others

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
-import { Route } from "react-router-dom";
+import { Route, Switch } from "react-router-dom";
 import MainHeader from "./components/MainHeader";
+import ProductDetail from "./screens/ProductDetail";
 import Products from "./screens/Products";
 import Welcome from "./screens/Welcome";
 
@@ -7,14 +8,17 @@ function App() {
   return (
     <div>
       <MainHeader />
-      <main>
+      <Switch>
         <Route path="/welcome">
           <Welcome />
         </Route>
-        <Route path="/products">
+        <Route path="/products" exact>
           <Products />
         </Route>
-      </main>
+        <Route path="/products/:productId">
+          <ProductDetail />
+        </Route>
+      </Switch>
     </div>
   );
 }
